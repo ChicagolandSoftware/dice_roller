39e0d5e18f864c6330307e698a228266a37919f6
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-02-03 07:10:08 -0600

    got d4 to work, fixed some stuff

--- background123.js
+++
@@ -1,5 +0,0 @@
-function openPage123() {
-  console.log("This is a stub");
-}
-
-browser.browserAction.onClicked.addListener(openPage123);
--- popup/roll.js
+++ popup/roll.js
@@ -1,3 +1,16 @@
-function test() {
-    document.writeln('test');
+function getRandomRoll(max) {
+    //getRandomRoll(6) means return 1-6
+    return Math.floor((Math.random() * Math.floor(max)) + 1);
+  }
+
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("d4").addEventListener("click", d4);
+
+function clear() {
+    document.getElementById("roll_result").innerHTML = "?";
 }
+
+function d4() {
+    document.getElementById("roll_result").innerHTML = getRandomRoll(4);
+}
+
