01c87275cbd97a9ca697d98c0138afed871fd749
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-02-03 08:18:41 -0600

    random roll was broken so I removed it for now, at least in the roll.js

--- popup/roll.js
+++ popup/roll.js
@@ -1,7 +1,8 @@
-function getRandomRoll(max) {
+function getRandomRoll(max_val) {
     //getRandomRoll(6) means return 1-6
-    return Math.floor((Math.random() * Math.floor(max)) + 1);
-  }
+    return Math.floor((Math.random() * Math.floor(max_val)) + 1);
+}
+
 
 //I used to have a single roll_dice(num) function but it didn't work?
 //the random numbers didn't behave as expected for some weird reason
@@ -26,6 +27,12 @@ function d20(){
     document.getElementById("roll_result").innerHTML = getRandomRoll(20);
 }
 
+function clear() {
+    document.getElementById("roll_result").innerHTML = "?";
+}
+
+
+//adding event listeners
 document.getElementById("clear").addEventListener("click", clear);
 document.getElementById("d4").addEventListener("click", d4);
 document.getElementById("d6").addEventListener("click", d6);
@@ -34,15 +41,3 @@ document.getElementById("d10").addEventListener("click", d10);
 document.getElementById("d12").addEventListener("click", d12);
 document.getElementById("d20").addEventListener("click", d20);
 
-function clear() {
-    document.getElementById("roll_result").innerHTML = "?";
-}
-
-
-function copy() {
-    var copyText = document.getElementById("roll_result");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-
-}
